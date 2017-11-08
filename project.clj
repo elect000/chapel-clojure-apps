d53e6b395126f36548fa1b94010cb61145a4c27b
elect000 <e.tmailbank@gmail.com>
2017-11-08 16:41:53 +0900

    second-commit

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot chapel-clojure-app.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.443"]
+                 [opencv/opencv "3.3.1"]
+                 [opencv/opencv-native "3.3.1"]]
+  :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)]
+  :main chapel-clojure-app.core
+)
